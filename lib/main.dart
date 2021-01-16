67036db7b888ed7715fbb05c695eda4002d14de2
mmcguire91 <mmcguire91@gmail.com>
2021-01-16 12:25:33 -0500

    added basic UI to Catalog Display Page

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 
+import './screens/catalog_display_screen.dart';
+
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,23 +14,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Shop App'),
-      ),
-      body: Center(
-        child: Text(
-          'Shop App text',
-        ),
-      ),
+      home: CatalogDisplayScreen(),
     );
   }
 }
---
+++ lib/widgets/product_item.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+class ProductItem extends StatelessWidget {
+  ProductItem({
+    this.id,
+    this.title,
+    this.imageURL,
+  });
+
+  final String id;
+  final String title;
+  final String imageURL;
+
+  @override
+  Widget build(BuildContext context) {
+    return GridTile(
+      child: Image.network(
+        imageURL,
+        fit: BoxFit.cover,
+      ),
+      footer: GridTileBar(
+        backgroundColor: Colors.black54,
+        leading: IconButton(
+          icon: Icon(Icons.favorite),
+          onPressed: () {},
+        ),
+        title: Text(
+          title,
+          textAlign: TextAlign.center,
+        ),
+        trailing: IconButton(
+          icon: Icon(Icons.shopping_cart),
+          onPressed: () {},
+        ),
+      ),
+    );
+  }
+}
