1e22a13388af214d5060cc1e3ac6de4900a2712b
Matt <mmcguire91@gmail.com>
2021-03-18 08:21:55 -0400

    pull to refresh on manage products view

--- lib/screens/manage_products_screen.dart
+++ lib/screens/manage_products_screen.dart
@@ -10,6 +10,11 @@ import '../models/products_provider.dart';
 
 //this is the screen to manage the products that will display
 
+Future<void> _refreshData(BuildContext context) async {
+  await Provider.of<ProductsProvider>(context, listen: false).getProducts();
+}
+//refresh the data by waiting on the response of the products provider to make the API call to ensure the data is consistent with the display
+
 class ManageProductsScreen extends StatelessWidget {
   static const routeName = '/manage_products';
   @override
@@ -30,19 +35,24 @@ class ManageProductsScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: ListView.builder(
-        itemCount: productData.items.length,
-        itemBuilder: (_, index) => Column(
-          children: [
-            ManageProductsItem(
-              id: productData.items[index].id,
-              title: productData.items[index].title,
-              imageURL: productData.items[index].imageURL,
-            ),
-            //this is built using the custom ManageProductsItem widget.
-            //The named arguments are pulling data from the products provider
-            Divider(),
-          ],
+      body: RefreshIndicator(
+        onRefresh: () => _refreshData(context),
+        //onRefresh property of RefreshIndicator requires a function that takes a Future so the property knows what it needs to wait on (and the duration of time)
+        color: Theme.of(context).primaryColor,
+        child: ListView.builder(
+          itemCount: productData.items.length,
+          itemBuilder: (_, index) => Column(
+            children: [
+              ManageProductsItem(
+                id: productData.items[index].id,
+                title: productData.items[index].title,
+                imageURL: productData.items[index].imageURL,
+              ),
+              //this is built using the custom ManageProductsItem widget.
+              //The named arguments are pulling data from the products provider
+              Divider(),
+            ],
+          ),
         ),
       ),
     );
