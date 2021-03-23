83a0c193c0850b0f92058e5348a603906c23c7ad
Matt <mmcguire91@gmail.com>
2021-03-23 11:02:10 -0400

    established API call to store the Favorited value of a product in the db

--- lib/models/product_model.dart
+++ lib/models/product_model.dart
@@ -1,5 +1,11 @@
 import 'package:flutter/foundation.dart';
 
+import 'package:http/http.dart' as http;
+//package for API calls
+//'as http' requires user to specify package.DataType
+import 'dart:convert';
+//json encode and decoder
+
 class Product with ChangeNotifier {
   Product({
     @required this.id,
@@ -19,9 +25,45 @@ class Product with ChangeNotifier {
   final String imageURL;
   bool isFavorite;
 
-  void toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
+//establishing void method to eliminate code duplication
+  void _setFavoriteStatus({bool favoriteValue}) {
+    isFavorite = favoriteValue;
     notifyListeners();
   }
-  //toggle the status of favorite button
+
+//Store the FAVORITE status in an API call to the db
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = isFavorite;
+    //storing the value of the old status in the case that the API call fails we may persist the old status to reflect to the user that the value has not been stored on the server
+    isFavorite = !isFavorite;
+    notifyListeners();
+    //toggle the status of favorite button
+    final url = Uri.https('shop-app-flutter-49ad1-default-rtdb.firebaseio.com',
+        '/products/$id.json');
+    //URL of the API call
+    //try to make the API call
+    try {
+      final response = await http.patch(
+        url,
+        body: json.encode({
+          'isFavorite': isFavorite,
+        }),
+        //encode the value of isFavorite variable to update the isFavorite field in the db
+      );
+      //store the response of the API call in the variable, response
+      //OPTIMISTIC UPDATING
+      if (response.statusCode >= 400) {
+        //if the API response returns an error
+        _setFavoriteStatus(
+          favoriteValue: oldStatus,
+        );
+        //reset the value of isFavorite to the isFavorite value that we're caching
+      }
+    } catch (error) {
+      _setFavoriteStatus(
+        favoriteValue: oldStatus,
+      );
+      //reset the value of isFavorite to the isFavorite value that we're caching
+    }
+  }
 }
